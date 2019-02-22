c3a7a8b4621ee1539de324477544329b45e28ee1
schonmann <antonio_schonmann@hotmail.com>
2019-02-21 23:30:17 -0300

    Unused import.

--- src/main/kotlin/br/com/schonmann/acejudgeserver/core/ security/SecurityConfig.kt
+++ src/main/kotlin/br/com/schonmann/acejudgeserver/core/ security/SecurityConfig.kt
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import java.util.*
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
