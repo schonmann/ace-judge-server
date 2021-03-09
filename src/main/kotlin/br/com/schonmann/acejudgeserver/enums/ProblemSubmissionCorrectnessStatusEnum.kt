2aca76765af36fb36434bd7c4e2e364e452f8926
schonmann <antonio_schonmann@hotmail.com>
2021-03-09 02:24:17 -0300

    Adding GENERIC_ERROR and removing comment

--- src/main/kotlin/br/com/schonmann/acejudgeserver/enums/ProblemSubmissionCorrectnessStatusEnum.kt
+++ src/main/kotlin/br/com/schonmann/acejudgeserver/enums/ProblemSubmissionCorrectnessStatusEnum.kt
@@ -2,11 +2,12 @@ package br.com.schonmann.acejudgeserver.enums
 
 enum class ProblemSubmissionCorrectnessStatusEnum(val status : Int) {
     JUDGE_QUEUE(0),
-    CORRECT_ANSWER(3), // WTF???
+    CORRECT_ANSWER(3),
     WRONG_ANSWER(1),
     WRONG_COMPLEXITY(2),
     RUNTIME_ERROR(3),
     COMPILE_ERROR(4),
     IN_EXECUTION(5),
-    TIME_LIMIT_EXCEEDED(6)
+    TIME_LIMIT_EXCEEDED(6),
+    GENERIC_ERROR(7),
 }
